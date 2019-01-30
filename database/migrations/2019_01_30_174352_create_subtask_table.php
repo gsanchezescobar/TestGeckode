cca1823ce1b191cb367271c854fc9c44259c0987
Guillermo Antonio Sanchez <gsanchezescobar@gmail.com>
2019-01-30 13:05:18 -0500

    se creo la base de datos

--- database/migrations/2019_01_30_174352_create_subtask_table.php
+++ database/migrations/2019_01_30_174352_create_subtask_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateSubtaskTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('subtask', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('task')->unsigned();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('description');
             $table->timestamps();
+            $table->foreign('task')->references('id')->on('task');
         });
     }
 
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('subtask');
     }
 }
